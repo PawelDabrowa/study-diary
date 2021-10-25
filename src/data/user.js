5de5a16d78f058d1db6676f1bd6c0555e013e0b3
Pawel Dabrowa <d@browa.co.uk>
2021-10-25 19:02:32 +0100

    Create items components

---
+++ src/data/user.js
@@ -0,0 +1,27 @@
+export const users = [
+  {
+    name: 'Pawel Dabrowa',
+    attendance: '39%',
+    average: '2.3',
+  },
+  {
+    name: 'Elle Thorpe',
+    attendance: '21%',
+    average: '4.3',
+  },
+  {
+    name: 'Amy Wilson',
+    attendance: '9%',
+    average: '1.3',
+  },
+  {
+    name: 'Rich Garson',
+    attendance: '66%',
+    average: '4.9',
+  },
+  {
+    name: 'Nick Colar',
+    attendance: '36%',
+    average: '3.5',
+  },
+]
